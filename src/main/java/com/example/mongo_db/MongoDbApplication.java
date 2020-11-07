e2b335230e628a87f1a452921228ea1903414cb9
Yuriy <tsarenkoyuriidev@gmail.com>
2020-11-07 14:59:11 +0200

    MASTER-1: initial commit.

---
+++ src/main/java/com/example/mongo_db/MongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongo_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbApplication.class, args);
+    }
+
+}
